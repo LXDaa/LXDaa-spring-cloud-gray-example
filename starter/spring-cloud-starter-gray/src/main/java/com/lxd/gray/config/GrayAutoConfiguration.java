a6b4842bf7e75bfb611300043b277d1ce99e767b
LXDa <2439185947@qq.com>
2025-03-21 21:10:37 +0800

    commit

--- starter/spring-cloud-starter-gray/src/main/java/com/lxd/gray/config/GrayAutoConfiguration.java
+++ starter/spring-cloud-starter-gray/src/main/java/com/lxd/gray/config/GrayAutoConfiguration.java
@@ -17,33 +17,39 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+// 定义配置类，使用@Configuration注解
 @Configuration
 // 可以通过@ConditionalOnProperty设置是否开启灰度自动配置 默认是不加载的
-@ConditionalOnProperty(value = "lxd.tool.gray.load",havingValue = "true")
+@ConditionalOnProperty(value = "lxd.tool.gray.load", havingValue = "true")
+// 启用GrayVersionProperties配置属性
 @EnableConfigurationProperties(GrayVersionProperties.class)
 public class GrayAutoConfiguration {
 
-
+    // 内部配置类，用于配置GrayGateway相关的过滤器和异常处理器
     @Configuration(proxyBeanMethods = false)
     @ConditionalOnClass(value = GlobalFilter.class)
     @EnableConfigurationProperties(GrayGatewayProperties.class)
     static class GrayGatewayFilterAutoConfiguration {
+        // 创建GrayGatewayBeginFilter实例
         @Bean
         public GrayGatewayBeginFilter grayGatewayBeginFilter() {
             return new GrayGatewayBeginFilter();
         }
+
+        // 创建GrayGatewayAfterFilter实例
         @Bean
         public GrayGatewayAfterFilter grayGatewayAfterFilter() {
             return new GrayGatewayAfterFilter();
         }
 
+        // 创建GrayGatewayExceptionHandler实例
         @Bean
-        public GrayGatewayExceptionHandler grayGatewayExceptionHandler(){
+        public GrayGatewayExceptionHandler grayGatewayExceptionHandler() {
             return new GrayGatewayExceptionHandler();
         }
     }
 
+    // 内部配置类，用于配置Spring MVC拦截器
     @Configuration(proxyBeanMethods = false)
     @ConditionalOnClass(value = WebMvcConfigurer.class)
     static class GrayWebMvcAutoConfiguration {
@@ -54,6 +60,7 @@ public class GrayAutoConfiguration {
         @Bean
         public WebMvcConfigurer webMvcConfigurer() {
             return new WebMvcConfigurer() {
+                // 重写addInterceptors方法，添加GrayMvcHandlerInterceptor拦截器
                 @Override
                 public void addInterceptors(InterceptorRegistry registry) {
                     registry.addInterceptor(new GrayMvcHandlerInterceptor());
@@ -62,6 +69,7 @@ public class GrayAutoConfiguration {
         }
     }
 
+    // 内部配置类，用于配置Feign拦截器
     @Configuration
     @ConditionalOnClass(value = RequestInterceptor.class)
     static class GrayFeignInterceptorAutoConfiguration {
